7d48d31232e6a699ea0e7e433f93578b45fb4993
Scarlett Wu <swu0207@berkeley.edu>
2016-02-17 22:09:13 -0800

    q1 finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   root 'pages#home'
+  post 'stringify', to: 'pages#stringify'
   get 'age', to: 'pages#age'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
